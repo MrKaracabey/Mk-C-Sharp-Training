d7ef9b3bd4ae9882541ca12c906bdfab70c58344
Mustafa KARACABEY <89645555+Mustafa-Karacabey@users.noreply.github.com>
2021-12-24 16:06:37 +0300

    [HelloWorld]

---
+++ Mk-C-Sharp-Training/101-basics/helloCSharp/helloWorld.cs
@@ -0,0 +1,9 @@
+namespace Mk_C_Sharp_Training._101_basics.helloCSharp;
+
+public class helloWorld
+{
+    public void helloCSharp()
+    {
+        Console.WriteLine("Hello C-Sharp I am Back");
+    }
+}
